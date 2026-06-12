72a1739b734696709142faf1f29e7ccd70002f17
cherylsingla <cherylsingla2007@gmail.com>
2026-06-12 16:36:02 +0530

    completed ttt bot using minmax

--- week2/q1.py
+++ week2/q1.py
@@ -10,7 +10,7 @@ logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s', datefmt=
 # Mapping from histories (str) to probability distribution over actions
 strategy_dict_x = {}
 strategy_dict_o = {}
-
+memo={}
 
 class History:
     def __init__(self, history=None):
@@ -76,26 +76,67 @@ class History:
     def is_win(self):
         # check if the board position is a win for either players
         # Feel free to implement this in anyway if needed
-        pass
-
+        # horizontal
+        for i in range(0,9,3):
+            if self.board[i]==self.board[i+1] and self.board[i+1]==self.board[i+2]:
+                if self.board[i]=="x":
+                    return "x"
+                elif self.board[i]=="o":
+                    return "o"
+        # vertical
+        for i in range(0,3,1):
+            if self.board[i]==self.board[i+3] and self.board[i+3]==self.board[i+6]:
+                if self.board[i]=="x":
+                    return "x"
+                elif self.board[i]=="o":
+                    return "o"   
+        # diagonal
+        if self.board[0]==self.board[4] and self.board[4]==self.board[8]:
+                if self.board[0]=="x":
+                    return "x"
+                elif self.board[0]=="o":
+                    return "o" 
+        # anti diagonal
+        if self.board[2]==self.board[4] and self.board[4]==self.board[6]:
+                if self.board[2]=="x":
+                    return "x"
+                elif self.board[2]=="o":
+                    return "o"
+        return "Not yet"         
+               
     def is_draw(self):
+        if "0" not in self.board and self.is_win()=="Not yet":
+            return True
+        return False
         # check if the board position is a draw
         # Feel free to implement this in anyway if needed
-        pass
 
     def get_valid_actions(self):
+        actions = []
+        for i in range(9):
+            if self.board[i] == "0":
+                 actions.append(i)
+        return actions
         # get the empty squares from the board
         # Feel free to implement this in anyway if needed
-        pass
 
     def is_terminal_history(self):
+        if self.is_win()=="x" or self.is_win()=="o" or self.is_draw()==True:
+            return True
+        return False
         # check if the history is a terminal history
         # Feel free to implement this in anyway if needed
-        pass
+
 
     def get_utility_given_terminal_history(self):
+        if self.is_terminal_history():
+            if self.is_win()=="x":
+                return 1
+            elif self.is_win()=="o":
+                return -1
+            else:
+                return 0
         # Feel free to implement this in anyway if needed
-        pass
 
     def update_history(self, action):
         # In case you need to create a deepcopy and update the history obj to get the next history object.
@@ -108,7 +149,7 @@ def backward_induction(history_obj):
     :param history_obj: Histroy class object
     :return: best achievable utility (float) for th current history_obj
     """
-    global strategy_dict_x, strategy_dict_o
+    global strategy_dict_x, strategy_dict_o,memo
     # TODO implement
     # (1) Implement backward induction for tictactoe
     # (2) Update the global variables strategy_dict_x or strategy_dict_o which are a mapping from histories to
@@ -116,15 +157,60 @@ def backward_induction(history_obj):
     # (2a)These are dictionary with keys as string representation of the history list e.g. if the history list of the
     # history_obj is [0, 4, 2, 5], then the key is "0425". Each value is in turn a dictionary with keys as actions 0-8
     # (str "0", "1", ..., "8") and each value of this dictionary is a float (representing the probability of
-    # choosing that action). Example: {”0452”: {”0”: 0, ”1”: 0, ”2”: 0, ”3”: 0, ”4”: 0, ”5”: 0, ”6”: 1, ”7”: 0, ”8”:
+    # choosing that action). Example: {"0452": {"0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 1, "7": 0, "8":
     # 0}}
     # (2b) Note, the strategy for each history in strategy_dict_x and strategy_dict_o is probability distribution over
     # actions. But since tictactoe is a PIEFG, there always exists an optimal deterministic strategy (SPNE). So your
     # policy will be something like this {"0": 1, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0} where
     # "0" was the one of the best actions for the current player/history.
-    return -2
     # TODO implement
-
+    his_str="".join(str(x) for x in history_obj.history)
+    if his_str in memo:
+        return memo[his_str]
+    prob_dis_o={"0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8":0}
+    prob_dis_x={"0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8":0}
+    if history_obj.is_terminal_history():
+        memo[his_str] = history_obj.get_utility_given_terminal_history()
+        return history_obj.get_utility_given_terminal_history()
+    if history_obj.current_player()=="x":
+        diff_x=history_obj.get_valid_actions()
+        utility=float('-inf')
+        action_value={}
+        for i in diff_x:
+            new_history = copy.deepcopy(history_obj)
+            new_history.history.append(i)
+            new_history.board = new_history.get_board()
+            action_value[i]=backward_induction(new_history)
+            utility=max(utility,action_value[i])
+        index=[]
+        for i in diff_x:
+            if utility==action_value[i]:
+                index.append(i)
+        for i in index:
+            prob_dis_x[f"{i}"]=1.0/len(index)
+        strategy_dict_x[his_str]=prob_dis_x
+        memo[his_str] = utility
+        return utility   
+    elif history_obj.current_player()=="o":
+        diff_o=history_obj.get_valid_actions()
+        utility=float('inf')
+        index=[]
+        action_value={}
+        for i in diff_o:
+            new_history = copy.deepcopy(history_obj)
+            new_history.history.append(i)
+            new_history.board = new_history.get_board()
+            action_value[i]=backward_induction(new_history)
+            utility=min(utility,action_value[i])
+        for i in diff_o:
+            if utility==action_value[i]:
+                index.append(i)
+        for i in index:
+            prob_dis_o[f"{i}"]=1.0/len(index)
+        strategy_dict_o[his_str]=prob_dis_o
+        memo[his_str] = utility
+        return utility 
+    
 
 def solve_tictactoe():
     backward_induction(History())
